f3e3316682ff4fc51aecb5190ecf0b20f62a06d4
Marek Rohon <marek.rohon@gmail.com>
2014-01-18 08:42:48 +0100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NeoBlog::Application.config.secret_token = 'c7e2948e12e3a35977c2ae8fe7477ace400d0f37dd1084aba4d7593f8b9385ee68b886a23f92243787876e2a91613e09a0ed5710513a4300467c04de180382cf'
